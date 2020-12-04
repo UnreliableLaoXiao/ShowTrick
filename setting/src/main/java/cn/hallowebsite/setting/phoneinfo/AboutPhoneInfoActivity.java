9c16fe22da593ec557c701f8b04f302675a4f8f4
xcl <heyxhazx1003@163.com>
2020-12-05 00:33:57 +0800

    【需求】【1/2】{实现关于手机的界面搭建和构想}
    分析：搭建代码，准备使用自定义View来实现一个手机内手机的显示效果
    方案：使用自定义View绘制一个手机样式，然后让点击区域高亮显示
    备注：可能需要后期重构优化

---
+++ setting/src/main/java/cn/hallowebsite/setting/phoneinfo/AboutPhoneInfoActivity.java
@@ -0,0 +1,17 @@
+package cn.hallowebsite.setting.phoneinfo;
+
+import cn.hallowebsite.lib.adapter.AbsActivity;
+import cn.hallowebsite.lib.router.ActivityRouter;
+
+@ActivityRouter(path = "Setiting/About/PhoneInfo")
+public class AboutPhoneInfoActivity extends AbsActivity {
+    @Override
+    protected void initOnCreate() {
+
+    }
+
+    @Override
+    protected int setLayoutXmlid() {
+        return 0;
+    }
+}
